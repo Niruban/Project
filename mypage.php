6400e68056dbe9c1bda1299c280f1df112e25b53
Niruban Thiyagarajan <niru.428@gmail.com>
2013-03-08 19:07:15 +0530

    io
    qwe

---
+++ mypage.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>This is my page</title> </head>
+<body <?php print "bgcolor=#FFFFFF"; ?>> <?php
+print "Hello!<br>";
+?>
+This is the content of my page.
+<?php
+print "Do you like it?";
+print "<br>";
+print "<b>If not, please visit a different page.</b>"; print "<br>";
+print "<h1>bye</h1>";
+?>
+</body> </html>
